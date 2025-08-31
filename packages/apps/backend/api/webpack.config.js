e7e1ebc025e0f55f83b06368d6d5a8fa8d257613
pandora2092 <garaga2092@yandex.ru>
2025-08-31 22:57:34 +0300

    nx build

--- packages/apps/backend/api/webpack.config.js
+++ packages/apps/backend/api/webpack.config.js
@@ -1,19 +1,32 @@
-const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
-const { join } = require('path');
+const { composePlugins, withNx } = require('@nx/webpack');
+const nodeExternals = require('webpack-node-externals');
 
-module.exports = {
-  output: {
-    path: join(__dirname, '../../../dist/apps/backend/api'),
-  },
-  plugins: [
-    new NxAppWebpackPlugin({
-      target: 'node',
-      compiler: 'tsc',
-      main: './src/main.ts',
-      tsConfig: './tsconfig.app.json',
-      assets: ['./src/assets'],
-      optimization: false,
-      outputHashing: 'none',
-    }),
-  ],
-};
+module.exports = composePlugins(withNx(), (config) => {
+  config.target = 'node';
+  config.externals = [nodeExternals()]; // исключает node_modules (оставляем только твой код)
+  config.output = {
+    ...config.output,
+    filename: 'main.js',
+  };
+  return config;
+});
+// const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
+// const { join } = require('path');
+
+// module.exports = {
+//   output: {
+//     path: join(__dirname, '../../../dist/apps/backend/api'),
+//   },
+//   plugins: [
+//     new NxAppWebpackPlugin({
+//       target: 'node',
+//       compiler: 'tsc',
+//       main: './src/main.ts',
+//       tsConfig: './tsconfig.app.json',
+//       assets: ['./src/assets'],
+//       optimization: false,
+//       outputHashing: 'none',
+//     }),
+//   ],
+// };
+
